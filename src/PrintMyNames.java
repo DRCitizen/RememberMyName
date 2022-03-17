5028e7215ed0f5fbe2e1de1d84df3d8fedd5a70b
Denis <denis.r94@yandex.com>
2022-03-17 23:15:54 +0200

    Initial commit

---
+++ src/PrintMyNames.java
@@ -0,0 +1,35 @@
+public class PrintMyNames {
+    public static void main(String[] args) {
+
+        String myName = "Denis";
+        String otherName = "I don't Have Friends";
+        String lastName = "Ruzhdiev";
+        int age = 27;
+        System.out.println(myName);
+        System.out.println(otherName.equals(myName));
+        System.out.println(otherName.compareTo(myName));
+        System.out.println(myName + " " + lastName);
+        System.out.println(age);
+        System.out.println(String.join(" ", myName, "Ruzhdiev", "27"));
+        System.out.print(otherName.concat(" ").concat("25"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+    }
+}
